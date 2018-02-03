892d146ba6e7ee2670a07ed0d79ac060641e83a7
mattinthecat <phoisgood1@gmail.com>
2018-02-03 01:36:12 -0800

    Scripts second try

---
+++ Pokemon AR/Assets/Scripts/SwipeDetection.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwipeDetection : MonoBehaviour {
+
+	public bool isSwiping;
+
+	public float minSwipeDistance;
+	public float errorRange;
+
+	public SwipeDetection direction = SwipeDirection.None;
+
+	public enum SwipeDirection {Right, Left, Up, Down, None};
+
+	private Touch intialTouch;
+
+	// Use this for initialization
+	void Start () {
+		Input.multiTouchEnabled = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.touchCount <= 0) {
+			return;
+		}
+
+		foreach (var touch in Input.touches) {
+			if (touch.phase == TouchPhase.Began) {
+				initialTouch = touch;
+			}
+
+	}
+}
