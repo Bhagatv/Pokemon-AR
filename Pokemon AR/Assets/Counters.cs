e67e925fd75c2e32bd02a2e2b85367c7cec8d425
TheAvianLord <angela-yixuan21@hotmail.com>
2018-02-03 13:28:29 -0800

    UI stuff

---
+++ Pokemon AR/Assets/Counters.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Counters : MonoBehaviour
+{
+    static int happiness = 0;
+    static int points = 0;
+    public Text happy;
+    public Text point;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        happy.text = happiness.ToString() + " / 100";
+        point.text = points.ToString() + " / 100";
+	}
+}
