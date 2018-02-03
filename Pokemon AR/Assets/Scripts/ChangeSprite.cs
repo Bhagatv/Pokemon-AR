2da585b19e1983920ed79cccf1c87aafe65107f8
TheKentrosaurus <kentaa98@gmail.com>
2018-02-03 15:14:16 -0800

    stuff

--- Pokemon AR/Assets/Scripts/ChangeSprite.cs
+++ Pokemon AR/Assets/Scripts/ChangeSprite.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class ChangeSprite : MonoBehaviour {
     public Sprite arrowNorth, arrowEast, arrowSouth, arrowWest;
-    int[] arrows = new int[30];
-
     public int index = 0;
+    public bool increaseIndex = false;
+
+    int[] arrows = new int[30];
     // Use this for initialization
     void Start ()
     {
@@ -16,40 +17,40 @@ public class ChangeSprite : MonoBehaviour {
     // Update is called once per frame
     void Update ()
     {
-        
-        while (index < 30)
-        {
-            Debug.Log(arrows[index]);
-            StartCoroutine(Wait(arrows[index]));
-        }
+        if (increaseIndex == true)
+            ArrowsMoving();
     }
 
     IEnumerator Wait(int arrow)
     {
         yield return new WaitForSeconds(5.0f);
-        index++;
         if (arrow == 0)
         {
-           this.GetComponent<SpriteRenderer>().sprite = arrowNorth;
+            Debug.Log("Changing to north");
+            this.GetComponent<SpriteRenderer>().sprite = arrowNorth;
         }
         else if (arrow == 1)
         {
+            Debug.Log("Changing to east");
             this.GetComponent<SpriteRenderer>().sprite = arrowEast;
         }
         else if (arrow == 2)
         {
+            Debug.Log("Changing to south");
             this.GetComponent<SpriteRenderer>().sprite = arrowSouth;
         }
         else if (arrow == 3)
         {
+            Debug.Log("Changing to west");
             this.GetComponent<SpriteRenderer>().sprite = arrowWest;
         }
     }
 
     void ArrowsMoving()
     {
+        increaseIndex = false;
         int idx = 0;
-        while (index < 30)
+        while (idx < 30)
         {
             Debug.Log(arrows[idx]);
             StartCoroutine(Wait(arrows[idx]));
