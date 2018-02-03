6ea6c7f788400b838220c69b8243de07a1850791
Veeral Bhagat <bhagatv@uci.edu>
2018-02-03 13:14:52 -0800

    all my changes boy

---
+++ Pokemon AR/Assets/pikaScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class pikaScript : MonoBehaviour
+{
+    public Button waterGun;
+    //public GameObject spawnPoint;
+    public GameObject waterSpray;
+    // Use this for initialization
+    void Start()
+    {
+
+        waterGun.onClick.AddListener(waterGunDown);
+        waterSpray.transform.Find("WaterShower").gameObject.SetActive(false);
+
+    }
+
+
+
+    IEnumerator Wait()
+    {
+        waterSpray.transform.Find("WaterShower").gameObject.SetActive(true);
+        yield return new WaitForSeconds(10);
+        waterSpray.transform.Find("WaterShower").gameObject.SetActive(false);
+
+    }
+    void waterGunDown()
+    {
+        StartCoroutine(Wait());
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
