3e8a19e844a8aee2af954441038ccf3cd50c350c
TheKentrosaurus <kentaa98@gmail.com>
2018-02-03 19:44:56 -0800

    kk

--- Pokemon AR/Assets/pikaScript.cs
+++ Pokemon AR/Assets/pikaScript.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 public class pikaScript : MonoBehaviour
 {
     public Button waterGun;
     //public GameObject spawnPoint;
     public GameObject waterSpray;
+    public Slider AIhealthbar;
     public GameObject explosion;
     // Use this for initialization
     void Start()
@@ -14,22 +16,44 @@ public class pikaScript : MonoBehaviour
 
         waterGun.onClick.AddListener(waterGunDown);
         waterSpray.transform.Find("WaterShower").gameObject.SetActive(false);
-
+        explosion.transform.gameObject.SetActive(false);
     }
 
 
 
     IEnumerator Wait()
     {
-        waterSpray.transform.Find("WaterShower").gameObject.SetActive(true);
-        yield return new WaitForSeconds(10);
-        waterSpray.transform.Find("WaterShower").gameObject.SetActive(false);
+        if (AIhealthbar.value > 0)
+        {
+            waterSpray.transform.Find("WaterShower").gameObject.SetActive(true);
+            yield return new WaitForSeconds(1);
+            if (AIhealthbar.value - 10 < 0)
+            {
+                AIhealthbar.value = 0;
 
+            }
+            else
+            {
+                AIhealthbar.value -= 10;
+            }
+            yield return new WaitForSeconds(10);
+            waterSpray.transform.Find("WaterShower").gameObject.SetActive(false);
+        }
+        else
+        {
+            //pika won
+            //show win board
+            //up points
+            explosion.transform.gameObject.SetActive(true);
+            yield return new WaitForSeconds(3);
+            SceneManager.LoadScene("Market");
+
+        }
     }
     void waterGunDown()
     {
         StartCoroutine(Wait());
-
+        
     }
 
     // Update is called once per frame
