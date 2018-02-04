7138fb94eba75d7494e3afbc755996f7c7513d34
Veeral Bhagat <bhagatv@uci.edu>
2018-02-03 16:50:48 -0800

    h

---
+++ Pokemon AR/Assets/Scripts/ChangeSprite.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeSprite : MonoBehaviour
+{
+    public Sprite arrowNorth, arrowEast, arrowSouth, arrowWest;
+    public bool gameIsOver = false;
+    public GameObject swipeDirection;
+
+    int arrow;
+    int num_losses = 0;
+    int num_wins = 0;
+
+    public int swipeNum;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        swipeNum = swipeDirection.GetComponent<SwipeDetection>().direction;
+        Debug.Log(swipeNum);
+        Debug.Log("TESTING");
+
+
+    }
+
+    IEnumerator Wait(int arrow)
+    {
+        gameIsOver = true;
+        if (arrow == 0)
+        {
+            this.GetComponent<SpriteRenderer>().sprite = arrowNorth;
+            this.GetComponent<SpriteRenderer>().color = Color.white;
+            yield return new WaitForSeconds(2);
+            if (swipeNum == 0)
+                this.GetComponent<SpriteRenderer>().color = Color.green;
+            else
+                this.GetComponent<SpriteRenderer>().color = Color.red;
+        }
+        else if (arrow == 1)
+        {
+            this.GetComponent<SpriteRenderer>().sprite = arrowEast;
+            this.GetComponent<SpriteRenderer>().color = Color.white;
+            yield return new WaitForSeconds(2);
+            if (swipeNum == 1)
+                this.GetComponent<SpriteRenderer>().color = Color.green;
+            else
+                this.GetComponent<SpriteRenderer>().color = Color.red;
+        }
+        else if (arrow == 2)
+        {
+            this.GetComponent<SpriteRenderer>().sprite = arrowSouth;
+            this.GetComponent<SpriteRenderer>().color = Color.white;
+            yield return new WaitForSeconds(2);
+            if (swipeNum == 2)
+                this.GetComponent<SpriteRenderer>().color = Color.green;
+            else
+                this.GetComponent<SpriteRenderer>().color = Color.red;
+        }
+        else if (arrow == 3)
+        {
+            this.GetComponent<SpriteRenderer>().sprite = arrowWest;
+            this.GetComponent<SpriteRenderer>().color = Color.white;
+            yield return new WaitForSeconds(2);
+            if (swipeNum == 3)
+                this.GetComponent<SpriteRenderer>().color = Color.green;
+            else
+                this.GetComponent<SpriteRenderer>().color = Color.red;
+        }
+        yield return new WaitForSeconds(1);
+        swipeDirection.GetComponent<SwipeDetection>().hasBeenSwiped = false;
+        swipeNum = -1;
+        gameIsOver = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        swipeNum = swipeDirection.GetComponent<SwipeDetection>().direction;
+        if (!gameIsOver)
+        {
+            arrow = Random.Range(0, 4);
+            Debug.Log(arrow);
+            StartCoroutine(Wait(arrow));
+      
+            IsGameOver();
+        }
+    }
+
+    void IsGameOver()
+    {
+        if (num_losses >= 10)
+            gameIsOver = true;
+        if (num_wins >= 3)
+            gameIsOver = true;
+    }
+
+
+}
--- Pokemon AR/Assets/Scripts/SwipeDetection.cs
+++ Pokemon AR/Assets/Scripts/SwipeDetection.cs
@@ -2,70 +2,80 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SwipeDetection : MonoBehaviour {
 
-	public bool isSwiping;
+public class SwipeDetection : MonoBehaviour
+{
+	public float minSwipeLength = 200f;
+	Vector2 isPress;
+	Vector2 notPress;
+	Vector2 deltaSwipe;
 
-	public float minSwipeDistance;
-	public float errorRange;
+	public static Swipe swipeDirection;
 
-	public SwipeDirection direction = SwipeDirection.None;
+	public enum Swipe { None, Up, Down, Left, Right };
+	public Transform Parent;
+	public bool currentlySwiping = false;
+	public int direction = 5;
+	public bool hasBeenSwiped = false;
 
-	public enum SwipeDirection {Right, Left, Up, Down, None};
 
-	private Touch initialTouch;
 
-	// Use this for initialization
-	void Start () {
-		Input.multiTouchEnabled = true;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (Input.touchCount <= 0) {
-			return;
-		}
-		print (direction);
-		foreach (var touch in Input.touches) {
-			if (touch.phase == TouchPhase.Began) {
-				initialTouch = touch;
-			} else if (touch.phase == TouchPhase.Moved) {
-				float deltaX = touch.position.x - initialTouch.position.x;
-				float deltaY = touch.position.y - initialTouch.position.y;
-				float swipeDistance = Mathf.Abs (deltaX) + Mathf.Abs (deltaY);
-				if (swipeDistance > minSwipeDistance && (Mathf.Abs (deltaX) > 0 || Mathf.Abs (deltaY) > 0)) {
-					isSwiping = true;
-
-					CalculateSwipeDirection (deltaX, deltaY);
-				}
-			}
-			else if (touch.phase == TouchPhase.Ended || touch.phase == TouchPhase.Canceled) {
-				initialTouch = new Touch ();
-				isSwiping = false;
-				direction = SwipeDirection.None;
-			}
-
-		}
+	void Start()
+	{
+		
 	}
 
-	void CalculateSwipeDirection(float deltaX, float deltaY){
-		bool isHorizontalSwipe = Mathf.Abs (deltaX) > Mathf.Abs (deltaY);
+	void Update ()
+	{
+		DetectSwipe();
 
-		if (isHorizontalSwipe && Mathf.Abs (deltaY) <= errorRange) {
-			if (deltaX > 0) {
-				direction = SwipeDirection.Right;
-			} else {
-				direction = SwipeDirection.Left;
+	}
+
+	public void DetectSwipe ()
+	{
+		if (Application.platform != RuntimePlatform.Android || Application.platform != RuntimePlatform.Android)
+		{
+			if (Input.GetMouseButtonDown (0)) {
+				isPress = new Vector2 (Input.mousePosition.x, Input.mousePosition.y);
+				currentlySwiping = true;
 			}
-		} else if (!isHorizontalSwipe && Mathf.Abs (deltaX) <= errorRange) {
-			if (deltaY > 0) {
-				direction = SwipeDirection.Up;
-			} else {
-				direction = SwipeDirection.Down;
+			if (Input.GetMouseButtonUp(0)) {
+				currentlySwiping = false;
+				notPress = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+				deltaSwipe = new Vector2 (notPress.x - isPress.x, notPress.y - isPress.y);
+				deltaSwipe.Normalize ();
 			}
-		} else {
-			isSwiping = false;
+
+			if (deltaSwipe.x > -0.5f && deltaSwipe.y > 0 && deltaSwipe.x < 0.5f && currentlySwiping && !hasBeenSwiped) {
+				Debug.Log ("up");
+				direction = 0;
+				hasBeenSwiped = true;
+
+			}
+			if (deltaSwipe.x > -0.5f && deltaSwipe.y < 0 && deltaSwipe.x < 0.5f && currentlySwiping && !hasBeenSwiped) {
+				Debug.Log ("down");
+				direction = 2;
+				hasBeenSwiped = true;
+			}
+			if (deltaSwipe.x < 0 && deltaSwipe.y > -0.5f && deltaSwipe.y < 0.5f && currentlySwiping && !hasBeenSwiped) {
+				Debug.Log ("left");
+				direction = 3;
+				hasBeenSwiped = true;
+			}
+			if (deltaSwipe.x > 0 && deltaSwipe.y > -0.5f && deltaSwipe.y < 0.5f && currentlySwiping && !hasBeenSwiped) {
+				Debug.Log ("right");
+				direction = 1;
+				hasBeenSwiped = true;
+			}
+
+
 		}
-	
+	  
+	}
+
+	IEnumerator Delay()
+	{
+		yield return new WaitForSeconds (5);
 	}
 }
+
