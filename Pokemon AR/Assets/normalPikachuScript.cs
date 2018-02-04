6f92f6c68732d36423734d49b1039ca9f0bc93e3
steve-ng562 <sjng1@uci.edu>
2018-02-03 17:09:36 -0800

    Normal Scene Done

---
+++ Pokemon AR/Assets/normalPikachuScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class normalPikachuScript : MonoBehaviour {
+    public GameObject pikachu;
+    public GameObject fireworks;
+    Animator animator;
+    	
+	
+    public void pikachuPat()
+    {
+       // fireworks.SetActive(false);
+        Debug.Log("button clicked");
+        animator = pikachu.GetComponent<Animator>();
+        StartCoroutine("Wait");
+        
+
+
+
+    }
+   IEnumerator Wait()
+    {
+        
+        animator.SetBool("success", true);
+        fireworks.SetActive(true);
+
+        yield return new WaitForSeconds(3.0f);
+        animator.SetBool("success", false);
+        fireworks.SetActive(false);
+        
+
+    }
+}
